8198e2f62ee940d6eb848466686b2161c641775f
Promise Ijeomah <promiseij4919@gmail.com>
2026-04-28 00:23:28 +0100

    built the refresh key state so that the amount on the ddahboard updates once there is a transaction

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,42 @@
-import { } from 'react'
-import{ BrowserRouter, Routes, Route } from "react-router-dom"
+import { useState } from 'react'
+import{ BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import { useAuth } from './context/AuthContext'
 import Dashboard from './pages/Dashboard'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
 import ProtectedRoute from './components/ProtectedRoute'
 import Layout from './components/Layout'
+import Transactions from './pages/Transactions'
 import './App.css'
 
 
+const RootRedirect  = () => {
+  const { user } = useAuth()
+  return <Navigate to={user? 'dashboard' : 'login'} replace/>
+}
 function App() {
+  const [refreshKey, setRefreshKey] = useState(0)
   return(
     <BrowserRouter>
     <Routes>
       {/* Public Routes */}
+      <Route path="/" element={<RootRedirect/>} />
        <Route path="/login" element={<Login/>} />
 
        <Route path="/signup" element={<Signup/>}/>
        {/* Protected Routes */}
-       <Route path="/dashboard"element={
+       <Route path="/dashboard" element={
         <ProtectedRoute>
-            <Layout>
-              <Dashboard />
+             <Layout onTransactionCreated={() => setRefreshKey(k => k + 1)}>
+              <Dashboard key={refreshKey} />
+            </Layout>
+           
+          </ProtectedRoute>}
+       />  
+       <Route path="/transactions"element={
+        <ProtectedRoute>
+            <Layout onTransactionCreated={() => setRefreshKey(k => k + 1)}>
+              <Transactions key={refreshKey} />
             </Layout>
            
           </ProtectedRoute>}

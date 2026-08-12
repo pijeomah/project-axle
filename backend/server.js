b0796caa663ee40c156013b4511519c317017f48
Promise Ijeomah <promiseij4919@gmail.com>
2026-08-12 13:11:34 +0100

    wired up CORS, helmet and morgan

--- backend/server.js
+++ backend/server.js
@@ -6,17 +6,24 @@ import transactionsV2Routes from './routes/v2/transactions.js'
 import walletsV2Routes from './routes/v2/wallets.js'
 import tagsV2Routes from './routes/v2/tags.js'
 import dashboardV2Routes from './routes/v2/dashboard.js'
+import helmet from 'helmet'
+import morgan from 'morgan'
 
 import  supabase  from './config/supabase.js'
 
 
 dotenv.config()
-
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://project-tally.vercel.app',
+]
  const app = express()
  const PORT = process.env.PORT || 5000
 
 
-app.use(cors())
+app.use(cors(
+  {origin: allowedOrigins,}
+))
 app.use(express.json())
 
 
